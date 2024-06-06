67664b6925d5ef9538803310223b5a7ecdbb8a8a
mateusz.szczepanski <mateusz.szczepanski@consult.red>
2024-06-06 15:19:15 +0200

    checkout task is done

---
+++ page-objects/ProductPage.js
@@ -0,0 +1,25 @@
+import { expect } from "@playwright/test"
+import { Navigation } from "./Navigation"
+
+export class ProductPage {
+    constructor(page) {
+        this.page = page
+        this.addButtons = page.locator('[data-qa="product-button"]')
+    }
+
+    visit = async () => {
+        await this.page.goto("/")
+    }
+
+    addProductToBasket = async (productIndex) => {
+        const theAddButton = await this.addButtons.nth(productIndex)
+        await theAddButton.waitFor()
+        await expect(theAddButton).toHaveText("Add to Basket")
+        const navigation = new Navigation(this.page)
+        const basketCountBeforeAdding = await navigation.getBasketCount()
+        await theAddButton.click()
+        await expect(theAddButton).toHaveText("Remove from Basket")
+        const basketCountAfterAdding = await navigation.getBasketCount()
+        await expect(basketCountAfterAdding).toBeGreaterThan(basketCountBeforeAdding)
+    }
+}
