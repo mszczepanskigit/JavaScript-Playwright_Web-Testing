265c195d1f5816d902c58d79d1fe195caa8efade
mateusz.szczepanski <mateusz.szczepanski@consult.red>
2024-06-07 14:57:06 +0200

    Registry data put into the /data/DeliveryDetailsInDict.js and imported to the main test file

---
+++ data/DeliveryDetailsInDicts.js
@@ -0,0 +1,20 @@
+import { generateRandomFiveDigitCode } from "../utils/GenerateDataUtils"
+
+export const deliveryDetails_save ={
+    firstName: "Tester John",
+    lastName: "Smith",
+    street: "Alabama Alley 69",
+    postCode: generateRandomFiveDigitCode(),
+    city: "Malmo",
+    country: "Sweden",
+    saveDecision: true
+}
+
+export const deliveryDetails_notSave ={
+    firstName: "Tester Yan",
+    lastName: "Kovalsky",
+    street: "Red Street 45",
+    postCode: generateRandomFiveDigitCode(),
+    city: "Minsk",
+    country: "Belarus"
+}
